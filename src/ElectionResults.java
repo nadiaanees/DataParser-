c0cb5bc142240b1d33486ecd149c68630e2264ff
nadiaanees <n.anees655@gmail.com>
2019-03-03 14:05:25 -0800

    created an ElectionResults class with 10 fields for each column in 2016_Presidential_Results.csv file (except the first)

---
+++ src/ElectionResults.java
@@ -0,0 +1,8 @@
+public class ElectionResults {
+
+    public ElectionResults(double votes_dem, double votes_gop, double total_votes, double per_dem, double per_gop, int diff, double per_point_diff, String state_abbr, String county_name, double combined_fips){
+
+    }
+
+
+}
